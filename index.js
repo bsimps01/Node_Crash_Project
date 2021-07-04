0b0a1c679ded37aadbfae052c0370a705d539dbd
bsimps01 <b.albertsimpson@gmail.com>
2021-07-04 13:47:05 -0700

    added files

---
+++ index.js
@@ -0,0 +1,16 @@
+// const Person = require('./person');
+// //import Person from './person'
+
+// const person1 = new Person('John Doe', 30);
+
+// person1.greeting();
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener', data));
+
+logger.log('Hello World');
+logger.log('Hi');
+logger.log('Hello');
